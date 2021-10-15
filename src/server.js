8e808f1c5245d057f3f4adf9a3a71d8beee1755d
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-15 12:32:25 -0300

    config express server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 const express = require ('express')
-const { resolve } = require('path')
-
 const app = express()
 
-app.use('/', express.static(
-    
-    resolve(__dirname,'./build')
-    
-    )
-)
+const baseDir = `${__dirname}/build/`
 
-app.listen(process.env.PORT || 3000, (err) => {
-    if (err) { return console.log(err)}
+app.use(express.static(baseDir))
+app.get('*', (request,response) => {
+    response.sendFile('index.html', {
+        root: baseDir
+    })
+})
 
-    console.log ('Tudo funcionando correto')
-} )
+
+// const express = require ('express')
+// const { resolve } = require('path')
+
+// const app = express()
+
+// app.use('/', express.static(
+    
+//     resolve(__dirname,'./build')
+    
+//     )
+// )
+
+// app.listen(process.env.PORT || 3000, (err) => {
+//     if (err) { return console.log(err)}
+
+//     console.log('Tudo funcionando correto')
+// } )
